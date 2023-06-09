ac51ae2e88575e828f90dad57fcaf130da614d1b
patjonstevenson <patjstevenson@icloud.com>
2023-06-08 22:59:07 -0400

    modify format of print_row

--- store/row.c
+++ store/row.c
@@ -48,6 +48,6 @@ void deserialize_row(void* source, Row* destination) {
 }
 
 void print_row(Row* row) {
-    printf("%d %s %s\n", row->id, row->username, row->email);
+    printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
